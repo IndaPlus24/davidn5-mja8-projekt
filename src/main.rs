7bb27f84698100785cda7651736ff68e33193c1d
David Nilsson <65786975+DavvveN@users.noreply.github.com>
2025-03-31 16:39:01 +0200

    draws a 10x20 grid

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,13 @@ use ggez::{
     Context, ContextBuilder, GameResult,
 };
 
+const BOARD_AMOUNT_COLUMNS: usize = 10;
+const BOARD_AMOUNT_ROWS: usize = 20;
+const BOARD_UPPER_LEFT: (i32, i32) = (100, 50);
+const BLOCK_SIZE: i32 = 25;
+
 struct AppState {
-    board: [[Block; 10]; 20], // Board is a 10 x 20 of blocks
+    board: [[Block; BOARD_AMOUNT_COLUMNS]; BOARD_AMOUNT_ROWS], // Board is a 10 x 20 of blocks
 }
 #[derive(Copy, Clone)]
 struct Block {
@@ -21,7 +26,8 @@ impl Block {
 impl AppState {
     fn new(ctx: &mut Context) -> GameResult<AppState> {
         let mut state = AppState {
-            board: [[Block::new(Color::RED); 10]; 20],
+            board: [[Block::new(Color::from_rgb(59, 59, 59)); BOARD_AMOUNT_COLUMNS];
+                BOARD_AMOUNT_ROWS],
         };
         Ok(state)
     }
@@ -35,6 +41,25 @@ impl event::EventHandler<ggez::GameError> for AppState {
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
         let mut canvas =
             graphics::Canvas::from_frame(ctx, graphics::Color::from([0.1, 0.2, 0.3, 1.0]));
+
+        for r in 0..BOARD_AMOUNT_ROWS {
+            for c in 0..BOARD_AMOUNT_COLUMNS {
+                let rectangle = graphics::Mesh::new_rectangle(
+                    ctx,
+                    graphics::DrawMode::fill(),
+                    graphics::Rect::new_i32(
+                        BOARD_UPPER_LEFT.0 + c as i32 * BLOCK_SIZE + 1,
+                        BOARD_UPPER_LEFT.1 + r as i32 * BLOCK_SIZE + 1,
+                        BLOCK_SIZE - 2,
+                        BLOCK_SIZE - 2,
+                    ),
+                    self.board[r][c].color,
+                )
+                .expect("COULDNT CREATE RECTANGLE FROM BLOCK");
+
+                canvas.draw(&rectangle, graphics::DrawParam::default());
+            }
+        }
 
         canvas.finish(ctx)?;
 
