877a1ff7d43396275813987d2d88ee07c9c658d5
David <david@sunnebo.se>
2025-04-05 10:09:05 +0200

    Fix: changes piece_type positions to be low to high

--- src/piece.rs
+++ src/piece.rs
@@ -8,7 +8,7 @@ pub enum PieceType {
     I, O, T, S, Z, J, L,
 }
 impl PieceType {
-    fn color(&self) -> Color {
+    pub fn color(&self) -> Color {
         match self {
             PieceType::I => Color::RED,
             PieceType::O => Color::RED,
@@ -28,15 +28,17 @@ pub struct Piece{
 }
 
 impl Piece{
+    // ALL PIECE TYPE POSITIONS ARE WRITTEN WITH "LOWEST" BLOCK FIRST
     fn get_block_positions (piece_type : PieceType) -> Vec<(usize,usize)>{
         match piece_type {
-            PieceType::I => vec![(3,0), (4,0), (5,0), (6,0)], // WRITTEN (C,R)
-            PieceType::J => vec![(3,0), (3,1), (4,1), (5,1)],
-            PieceType::L => vec![(3,0), (3,1), (4,0), (5,0)],
-            PieceType::O => vec![(4,0), (4,1), (5,0), (5,1)],
-            PieceType::S => vec![(3,1), (4,0), (4,1), (5,0)],
-            PieceType::Z => vec![(3,0), (4,0), (4,1), (5,1)],
-            PieceType::T => vec![(3,0), (4,0), (4,1), (5,0)],
+            PieceType::I => vec![(3,0), (4,0), (5,0), (6,0)], // WRITTEN (C, R)
+            PieceType::J => vec![(3,1), (4,1), (5,1), (3,0)], 
+            PieceType::L => vec![(3,1), (3,0), (4,0), (5,0)], 
+            PieceType::O => vec![(4,1), (5,1), (4,0), (5,0)], 
+            PieceType::S => vec![(3,1), (4,1), (4,0), (5,0)], 
+            PieceType::Z => vec![(4,1), (5,1), (3,0), (4,0)], 
+            PieceType::T => vec![(4,1), (3,0), (4,0), (5,0)], 
+            
         }
     } 
     
